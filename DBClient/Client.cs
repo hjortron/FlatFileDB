d7c7dbc58c7ed062e755400376701a3dd8f454aa
hjortron <percunium@gmail.com>
2015-07-12 01:57:46 +0300

    SignalR

---
+++ DBClient/Client.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using System.Net.Http;
+using System.Timers;
+
+namespace DBClient
+{
+    class Client
+    {
+        public static HubConnection Connection { get; set; }
+        public static IHubProxy HubProxy { get; set; }
+        private static System.Timers.Timer timer;
+
+        public static async void ConnectAsync()
+        {
+            Connection = new HubConnection("http://localhost:8080");
+            Connection.Closed += Connection_Closed;
+            HubProxy = Connection.CreateHubProxy("MyHub");                    
+            try
+            {
+                await Connection.Start();
+            }
+            catch (HttpRequestException)
+            {
+                WriteMessage("Unable to connect to server: Start server before connecting clients.");            
+                return;
+            }
+
+            WriteMessage("Connected to server\r");           
+        }
+
+        public static void StartSending()
+        {
+            timer = new System.Timers.Timer(60);
+            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            WriteMessage("Start sending records");
+            timer.Start();
+        }
+
+        public static void StopSending()
+        {
+            WriteMessage("Stop sending records");
+            timer.Stop();
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            SendRecord();
+        }
+
+        public static void SendRecord()
+        {
+            var random = new Random(); 
+            var dataLength = 128;
+            byte[] array = new byte[dataLength * random.Next(1, 3)];
+            random.NextBytes(array);            
+            Client.HubProxy.Invoke("WriteRecord", random.Next(1, 1000), random.Next(1, 10), array);
+        }  
+        
+        public static void GetRecords()
+        {
+            var random = new Random();
+            var randomQuery = String.Format("sourceid = {0} AND sourcetype = {1}", random.Next(1, 1000), random.Next(1, 10));
+            WriteMessage("Get " + randomQuery);
+            Client.HubProxy.Invoke("GetRecords", randomQuery);
+        }
+
+        private static void WriteMessage(string message)
+        {
+            var dispatcher = Application.Current.Dispatcher;
+            dispatcher.Invoke(() => ((MainWindow)Application.Current.MainWindow).WriteToConsole(message));
+        }
+
+        private static void Connection_Closed()
+        {
+            WriteMessage("Disconnected!");
+        }
+    }
+}
---
+++ FlatFileDB/Server.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace FlatFileDB
+{
+    class Server
+    {
+        public static IDisposable SignalR { get; set; }
+
+        public static void StartServer()
+        {
+            try
+            {
+                SignalR = WebApp.Start("http://localhost:8080");
+            }
+            catch (TargetInvocationException)
+            {
+                //Dispatcher.Invoke(() =>
+                //Console.WriteLine("A server is already running"));
+                //Dispatcher.Invoke(() => ((MainWindow)Application.Current.MainWindow).buttonStart.IsEnabled = true);
+                //return;
+            }
+            //Dispatcher.Invoke(() => .buttonStop.IsEnabled = true));
+            //Dispatcher.Invoke(() =>
+            //((MainWindow)Application.Current.MainWindow).WriteToConsole("Server started at " + serverName));
+        }             
+    }
+ 
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    
+    public class MyHub : Hub
+   {        
+        public void WriteRecord(int sourceId, int sourceType, byte[] data)
+        {           
+            Program.dbService.AddRecord(sourceId, sourceType, data);
+        }
+
+        public void GetRecords(string query)
+        {
+            var records = Program.dbService.Read(query);
+            Clients.Caller.WriteRecords(records);
+        }
+        public override Task OnConnected()
+        {
+            //Use Application.Current.Dispatcher to access UI thread from outside the MainWindow class
+            Console.WriteLine("Client connected: " + Context.ConnectionId);
+
+            return base.OnConnected();
+        }
+        public void OnDisconnected()
+        {
+            //Use Application.Current.Dispatcher to access UI thread from outside the MainWindow class
+            Console.WriteLine("Client disconnected: " + Context.ConnectionId);            
+        }
+    }
+}
