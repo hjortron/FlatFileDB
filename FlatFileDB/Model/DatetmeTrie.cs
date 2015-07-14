a82c865ede1fe2c496e1d9183f77ca86fd0c8c1e
hjortron <percunium@gmail.com>
2015-07-14 11:39:02 +0300

    Trie

--- FlatFileDB/Model/DatetmeTrie.cs
+++ FlatFileDB/Model/DatetmeTrie.cs
@@ -10,44 +10,57 @@ namespace FlatFileDB.Model
     {
         private TrieNode _rootNode;
 
-        public void AddRecord(int[] dateParts, long[] recordsPos)
+        public DatetmeTrie()
         {
+            _rootNode = new TrieNode(0);
+        }
 
+        public void AddRecord(IEnumerable<int> dateParts, long[] recordsPos)
+        {
+            _rootNode.AddRecord(dateParts, recordsPos);
         }
 
         private class TrieNode
         {
-            private int datePart;
-            private IEnumerable<long> recordsPositions;
+            private int _datePart;
+            private IEnumerable<long> _recordsPositions;
             private Dictionary<int, TrieNode> edges;
 
-            private void AddRecord(IEnumerable<int> dateParts, long[] recordsPos)
+            internal TrieNode(int newDatePart)
+            {
+                _datePart = newDatePart;
+                edges = new Dictionary<int, TrieNode>();
+            }
+
+            internal void AddRecord(IEnumerable<int> dateParts, long[] recordsPos)
             {
                 if (dateParts.Count() == 1)
                 {
-                    recordsPositions.Concat(recordsPos);
+                    _recordsPositions = _recordsPositions.Concat(recordsPos);
                 }
                 else
                 {
+                    var nodeKey = dateParts.First();
                     TrieNode nextNode;
-                    if (edges.TryGetValue(dateParts.First(), out nextNode))
+                    if (edges.TryGetValue(nodeKey, out nextNode))
                     {
                         nextNode.AddRecord(dateParts.Skip(1), recordsPos);
                     }
                     else
                     {
+                        nextNode = new TrieNode(nodeKey);
                         nextNode.AddRecord(dateParts.Skip(1), recordsPos);
-                        edges.Add(dateParts.First(), nextNode);
+                        edges.Add(nodeKey, nextNode);
                     }
-                        }
-            }
+                }
+            }           
 
             private IEnumerable<long> GetRecordsPositions()
             {
                 var resultList = new List<long>();
                 if (!edges.Any())
                 {
-                    return recordsPositions;
+                    return _recordsPositions;
                 }
                 foreach (var edge in edges)
                 {
